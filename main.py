ac5db665d1d774c6f0dcaf708101220ff1ce641a
mathematiger <koehler.dominik@upb.de>
2024-09-15 14:36:59 +0200

    Update Dependencies

--- main.py
+++ main.py
@@ -11,30 +11,39 @@ dlsr = DLSyntaxObjectRenderer()
 # setup for parameters
 
 retrain_GNN_and_data = False
+list_datasets = ['house']
+dict_types_to_classify = {'house': 'B'}
+gnn_parameters = [{'dataset': 'house',
+                  'name': 'SAGE_2_20', 'gnn_layers': 2, 'epochs': 20}]
 
 
 # ------- Code -----------
-gnn_cl, dataset, dataset_class = create_gnn_and_dataset('house_1000',
-                                                        'SAGE_2_20',
-                                                        gnn_epochs=20,
-                                                        gnn_layers=2,
-                                                        type_to_classify='B',
-                                                        retrain=retrain_GNN_and_data,
-                                                        )
+gnns = dict()
+data = dict()
+data_cl = dict()
 
+for ds in list_datasets:
+    for gnnparams in gnn_parameters:
+        gnns[ds], data[ds], data_cl[ds] = create_gnn_and_dataset(dataset_name=ds,
+                                                                 gnn_name=gnnparams['name'],
+                                                                 gnn_epochs=gnnparams['epochs'],
+                                                                 gnn_layers=gnnparams['gnn_layers'],
+                                                                 type_to_classify=dict_types_to_classify[ds],
+                                                                 retrain=retrain_GNN_and_data
+                                                                 )
 
-# beam search CEs
-beam_search = BeamSearch(gnn_cl.model, dataset,
-                         beam_width=100, beam_depth=12, max_depth=2)
-beam = beam_search.beam_search()
+        # beam search CEs
+        beam_search = BeamSearch(gnns[ds].model, data[ds],
+                                 beam_width=100, beam_depth=12, max_depth=2)
+        beam = beam_search.beam_search()
 
+        # return top CEs
+        test_ce_dataset = create_test_dataset(num_nodes=100)
+        fideval = FidelityEvaluator(
+            test_ce_dataset, gnns[ds].model, type_to_explain='B')
+        print(f"Top 10 CEs: of dataset {ds} and gnn {gnnparams['name']}")
+        for i in range(10):
 
-# return top CEs
-test_ce_dataset = create_test_dataset(num_nodes=100)
-fideval = FidelityEvaluator(test_ce_dataset, gnn_cl.model, type_to_explain='B')
-
-print("Top 10 CEs:")
-for i in range(10):
-    acc = fideval.score_fid_accuracy(beam[i])
-    print(
-        f"Number {i+1} CE is {dlsr.render(beam[i])} has an acc of {round(acc, 2)}")
+            acc = fideval.score_fid_accuracy(beam[i])
+            print(
+                f"Number {i+1} CE is {dlsr.render(beam[i])} has an acc of {round(acc, 2)}")
